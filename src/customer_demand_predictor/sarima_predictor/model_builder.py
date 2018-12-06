174a43344a7120f3d9d04a9be8f3adaa1d23dd06
Peter Kings <p.kings@icloud.com>
2018-12-06 16:52:25 +0100

    reset index on filtered dataframe for customer (causes error in x13 if index is not regularly)

--- src/customer_demand_predictor/sarima_predictor/model_builder.py
+++ src/customer_demand_predictor/sarima_predictor/model_builder.py
@@ -6,8 +6,9 @@ def train_models_for_each_customer(df_consumption_and_production):
     customers_to_train_models = list(df_consumption_and_production['customerName'].unique())
 
     for customer in customers_to_train_models:
-        df_customer = df_consumption_and_production[df_consumption_and_production['customerName'] == customer]
+        df_customer = df_consumption_and_production[df_consumption_and_production['customerName'] == customer].reset_index()
         series_customer = df_customer['kWh'][-96:]  # train on last 96 timesots
+        print(series_customer.index)
         time_series = TimeSeries(series_customer, 24, False, cdp.MODEL_DIR, cdp.SARIMA_MODEL_SUFFIX, name=customer)
         best_orders = time_series.find_best_orders()
         print(best_orders)
