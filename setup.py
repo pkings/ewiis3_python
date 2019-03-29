08540425cf0bacdc7a0a583235d09eac3c3c19fb
Peter Kings <p.kings@icloud.com>
2019-03-29 11:11:21 +0100

    rename package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='customer_demand_predictor',
+    name='ewiis3_python_scripts',
     packages=find_packages('src'),
     version='0.0.1',
-    description='Analyse and predict the demand of a brokers customer',
+    description='Analyse and predict for ewiis3 broker',
     author='Peter Kings',
     package_dir={"": "src"},
 )
